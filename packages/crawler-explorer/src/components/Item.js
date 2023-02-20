806bc4da6cb155c545f564a713249ef678f3e616
R13 <rbentos@yahoo.com>
2023-02-20 02:22:02 -0300

    Explorer upgrade to next.js 13
    npm install next@latest react@latest --save
    npm update --save

--- packages/crawler-explorer/src/components/Item.js
+++ packages/crawler-explorer/src/components/Item.js
@@ -3,9 +3,7 @@ import { List } from 'semantic-ui-react'
 export default function Item({ url, onClick, children }) {
 	return (
 		<List.Item className='Anchor' onClick={() => onClick(url)}>
-			{/* <a href={url}> */}
 			{children}
-			{/* </a> */}
 		</List.Item>
 	);
 }
